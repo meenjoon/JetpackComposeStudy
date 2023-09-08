6c872c2aaa96260c5cf220e9a88054be81803640
meenjoon <zqudwnsz@naver.com>
2023-09-08 14:37:08 +0900

    feat: Card 학습 추가

---
+++ app/src/main/java/com/mbj/composestudy/card/Card.kt
@@ -0,0 +1,130 @@
+package com.mbj.composestudy.card
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.mbj.composestudy.ui.theme.JetpackComposeStudyTheme
+
+class Card : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeStudyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column {
+                        CardEx(cardData)
+                        CardEx(cardData)
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        val cardData = CardData(
+            imageUri = "https://raw.githubusercontent.com/Fastcampus-Android-Lecture-Project-2023/part4-chapter3/main/part4-chapter3-10/app/src/main/res/drawable-xhdpi/wall.jpg",
+            imageDescription = "엔텔로프 캐년",
+            author = "Dalinaum",
+            description = "엔텔로프 캐년은 죽기 전에 꼭 봐야할 절경으로 소개되었습니다."
+        )
+    }
+}
+
+@Composable
+fun CardEx(cardData: CardData) {
+    val placeHolderColor = Color(0x33000000)
+
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        modifier = Modifier.padding(4.dp),
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(8.dp)
+        ) {
+            // 스텝 1: `AsyncImage`, `Spacer`, `Column`, `Text`로
+            // 레이아웃을 만들어보세요.
+//            AsyncImage(
+//                model = cardData.imageUri,
+//                contentDescription = cardData.imageDescription,
+//                modifier = Modifier.size(32.dp)
+//            )
+//            Spacer(modifier = Modifier.size(8.dp))
+//            Column {
+//                Text(
+//                    text = cardData.author
+//                )
+//                Spacer(modifier = Modifier.size(4.dp))
+//                Text(
+//                    text = cardData.description
+//                )
+//            }
+
+            // 스텝 2: `AsyncImage`에는 `placeholder`를 지정하고,
+            // `contentScale`을 `ContentScale.Crop`으로 설정합시다.
+            // `clip(CircleShape)`로 둥근 외양을 만들어 봅시다.
+            AsyncImage(
+                model = "https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=11288788&filePath=L2Rpc2sxL25ld2RhdGEvMjAxNS8wMi9DTFM2OS9OVVJJXzAwMV8wMjc0X251cmltZWRpYV8yMDE1MTIwMw==&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10006",
+                placeholder = ColorPainter(placeHolderColor),
+                contentScale = ContentScale.Crop,
+                contentDescription = "엔텔로프 캐년",
+                modifier = Modifier
+                    .size(32.dp)
+                    .clip(CircleShape)
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Column {
+                Text(
+                    text = cardData.author
+                )
+                Spacer(modifier = Modifier.size(4.dp))
+                Text(
+                    text = cardData.description
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeStudyTheme {
+        Row {
+            CardEx(Card.cardData)
+        }
+    }
+}
+
+data class CardData(
+    val imageUri: String,
+    val imageDescription: String,
+    val author: String,
+    val description: String
+)
